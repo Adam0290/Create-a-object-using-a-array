5e1d96385d7c03e6c2ee2a1e0d1450d63a4dec3d
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-04-21 16:12:51 +0100

    Add files via upload

---
+++ object_javascript_1/script.js
@@ -0,0 +1,9 @@
+// Create a object using Javascript 1. //
+
+let ford = {
+
+brand: "Ford", // brand: = The name of the property.
+model: "Ferrari", // "Ford" = the value of the property.
+price: 12345224,
+color: "white",
+};
